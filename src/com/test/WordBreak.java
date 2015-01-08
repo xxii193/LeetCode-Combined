e4edc0428023c50902effa58137a4a15af4ce2f8
xxii193 <xxii193@gmail.com>
2015-01-08 23:05:56 +0800

    update add maxLength method

--- src/com/test/WordBreak.java
+++ src/com/test/WordBreak.java
@@ -2,6 +2,13 @@ package com.test;
 import java.util.*;
 
 public class WordBreak {
+	public int maxLength(Set<String> dict){
+		int max = 0;
+		for(String str:dict){
+			max = Math.max(str.length(), max);
+		}
+		return max;
+	}
     public boolean wordBreak(String s, Set<String> dict) {
     	int length = s.length();
     	Boolean[] checked = new Boolean[length + 1];
@@ -9,7 +16,7 @@ public class WordBreak {
     	checked[0] = true;
        for(int i = 1; i < length + 1; i++){
     	   checked[i] = false;
-    	   for(int j = 1; j <= i; j++){
+    	   for(int j = 1; j <= i && j <= maxLength(dict); j++){
     		   if(checked[i - j] == true && dict.contains(s.substring(i - j,i))){
     				   checked[i] = true;
         			   break;
