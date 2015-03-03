f0dd8173367741bc26f5316bcaf81c693e8f1985
unknown <XGong2@bgc-xgong2.DIR.slb.com>
2015-03-03 19:22:21 +0800

    add case convert

--- src/com/test/ConvertSortedListToBST.java
+++ src/com/test/ConvertSortedListToBST.java
@@ -1,15 +1,38 @@
 package com.test;
-
+/*
+ * treenode = list的中间节点
+ * treenode.left = list左半部分的中间节点
+ * treenode.right = list右半部分的中间节点、
+ * */
 public class ConvertSortedListToBST {
     public TreeNode sortedListToBST(ListNode head) {
         
          if(head == null)
          	return null;
-         middleNode(head, null);
+         if(head.next == null)
+        	 return new TreeNode(head.val);
+         
+         ListNode beforeMid = middleNode(head);
+         ListNode end = beforeMid.next.next;
+         TreeNode root = new TreeNode(beforeMid.next.val);
+         beforeMid.next = null;
+         root.left = sortedListToBST(head);
+         root.right = sortedListToBST(end);
+         
+         return root;
         
     }
     
-    public TreeNode middleNode(ListNode start, int len){
-    
+    public ListNode middleNode(ListNode start){
+        if(start.next == null)
+        	return start;
+    	ListNode fast = start;
+        ListNode slow = start;
+    	while(fast.next != null && fast.next.next != null){
+    		fast = fast.next.next;
+    		start = slow;
+    		slow = slow.next;
+    	}
+    	return start;
     }
 }
