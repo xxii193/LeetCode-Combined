662f735387fa43e3f8878d3e8cb018f58d38b537
unknown <XGong2@bgc-xgong2.DIR.slb.com>
2015-03-04 17:54:45 +0800

    add case countandsay

--- src/com/test/CountAndSay.java
+++ src/com/test/CountAndSay.java
@@ -9,10 +9,48 @@ The count-and-say sequence is the sequence of integers beginning as follows:
 Given an integer n, generate the nth sequence.
 
 Note: The sequence of integers will be represented as a string.
+
+思路：
+f(n) = 
+遍历f(n-1)
+  if(同一个数字)
+         个数累加
+  if(不同的数字)
+          返回上个数字的个数+数字 
  */
 public class CountAndSay {
     public String countAndSay(int n) {
+        if(n == 1)
+        	return "1";
+        String pre = countAndSay(n - 1);
         
-    	return "";
+        int count = 1;
+        char c = pre.charAt(0);
+        String result = "";
+        if(pre.length() == 1){
+        	result = "1"+c;
+        	return result;
+        }
+        	
+        for(int i = 1; i < pre.length(); i++){
+        		if(pre.charAt(i) != c){
+        			result += ( String.valueOf(count) + c );
+        			c = pre.charAt(i);
+        			count = 1;
+        		}
+        		else{
+        			count ++;
+        			
+        		}
+        	
+        }
+        result += (String.valueOf(count) + c);
+        return result;
+    }
+    
+    
+    public static void main(String[] args ){
+    	CountAndSay t = new CountAndSay();
+    	System.out.println(t.countAndSay(4));
     }
 }
